598352d4cd173abe0b2e447369e24f48d12d5490
Alex Stulen <ookno16@gmail.com>
2023-02-26 14:24:35 +0200

    Feature: added process request user call

--- src/assets/js/callBlock.js
+++ src/assets/js/callBlock.js
@@ -1,3 +1,23 @@
+function reCaptchResponse(response){
+	const $recaptchaInput = document.querySelector('#id_call_block_recaptcha_input')
+	$recaptchaInput.value = response
+}
+
+function reCaptchErrorResponse(response){
+	const $recaptchaInput = document.querySelector('#id_call_block_recaptcha_input')
+	$recaptchaInput.value = ''
+}
+
+var onloadCallback = function() {
+    grecaptcha.render('id_call_block_recaptcha_container', {
+      'sitekey' : '6LdAMZIkAAAAAIU0ohinOyUuoOe_Y9y43jvl346B',
+      'callback': reCaptchResponse,
+      'error-callback': reCaptchErrorResponse,
+      'hl': 'uk'
+    });
+  };
+
+
 const buttonCall = document.querySelector('.button.small')
 const callBlockContent = document.querySelector('.call-block__content')
 const callBlock = document.querySelector('.call-block')
@@ -33,4 +53,51 @@ function OpenCallBlock(obj) {
 }
 function CloseCallBlock(obj) {
 	obj.classList.remove('open')
+}
+
+async function sendReuqestPhoneCall(phone, recaptcha){
+	const url = `${URLS.request_call.api_uri}/request-calls/`
+	const data = {
+		phone: phone,
+		recaptcha: recaptcha
+	}
+	console.log(data)
+	return await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	})
+}
+
+
+async function processCallModalRequest(form){
+	const $phone = form.querySelector('#phone')
+
+	const $recaptcha = form.querySelector('#id_call_block_recaptcha_input')
+	const $recaptcha_error = form.querySelector('#id_call_block_recaptch_error')
+	$recaptcha_error.innerHTML =  !$recaptcha.value ? 'Це поле обов`язкове!' : ''
+	if(!$recaptcha.value){
+		return
+	}
+
+	const response = await sendReuqestPhoneCall($phone.value, $recaptcha.value)
+	
+	if(response.status !== 201){
+        Swal.fire({
+            icon: 'error',
+            title: 'Ooops..',
+            text: 'Невдалося відправити дані. Спробуйте ще раз.'
+        })
+    }
+    else{
+        Swal.fire({
+            icon: 'success',
+            title: 'Успішно',
+            text: 'Дякуємо! Ваша заявка прийнята. Ми з вами зв`яжимося як найшвидше.'
+        })
+    }
+
+	return false
 }
