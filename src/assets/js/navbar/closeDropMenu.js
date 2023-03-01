fbcb1ad39807f69bf9609fd04419d3bdbd31cb9c
Nazar Danilov <veemy23@gmail.com>
2023-03-01 17:27:42 +0200

    Fixed bugs with body overflow hidden

--- src/assets/js/navbar/closeDropMenu.js
+++ src/assets/js/navbar/closeDropMenu.js
@@ -2,5 +2,10 @@ const listItems = document.querySelectorAll('.mega_m > ul > li')
 listItems.forEach(el => {
 	el.addEventListener('click', (e) => {
 		document.querySelector('.menu').classList.remove('menu_state_open')
+		if (document.querySelector('.menu').classList.contains('menu_state_open')) {
+			document.body.style.overflow = 'hidden'
+		} else {
+			document.body.style.overflow = 'auto'
+		}
 	})
 });
