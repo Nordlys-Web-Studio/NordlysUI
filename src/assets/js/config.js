f759999b6b4dfc51736d6e087106078beba4c796
Nazar Danilov <veemy23@gmail.com>
2023-03-02 21:20:36 +0200

    An attempt to make site cards dynamic

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -5,23 +5,28 @@ const API_URL = `/api/${API_VERSION}`
 const API_URI = SERVER_HOST + API_URL
 
 const URLS = {
-    contacts: {
-        api_uri: `${API_URI}/contacts/api/v1`
-    },
-    request_call: {
-        api_uri: `${API_URI}/request-calls/api/v1`
-    }
+	contacts: {
+		api_uri: `${API_URI}/contacts/api/v1`
+	},
+	request_call: {
+		api_uri: `${API_URI}/request-calls/api/v1`
+	}
 }
 
 const SITE_TYPE_DB_ID = 1
 
 
 function setPopovers() {
-    const popoverTriggerList = document.querySelectorAll(
-      '[data-bs-toggle="popover"]'
-    );
-    return [...popoverTriggerList].map(
-      (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
-    );
-  }
-  
+	const popoverTriggerList = document.querySelectorAll(
+		'[data-bs-toggle="popover"]'
+	);
+	return [...popoverTriggerList].map(
+		(popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+	);
+}
+
+
+async function getSiteTypes() {
+	const url = `${API_URI}/price-calculation/rows/${SITE_TYPE_DB_ID}/`;
+	return await fetch(url);
+}
